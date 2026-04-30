347b627915625913559f1025a32178a6ed6bcfb5
TARS-90 <grzywiastyyy@gmail.com>
2026-04-30 11:51:55 +0200

    Refactoring adding new node to snake

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <unistd.h>
+#include <time.h>
 
 #define SIZE 20	// size of one block 
 #define WIN_WIDTH  900
@@ -17,49 +18,82 @@ struct Game {
 struct Node {
 	int x;
 	int y;
+	enum Direction direction;
 	struct Node* next;
 };
 
 struct Snake {
 	struct Node* head;
-	enum Direction direction;	
+	struct Node* tail;
 	int length;  
 };
 
-void add_node(struct Snake* snake, int x, int y) {
-	struct Node* tmp = snake->head;
-	struct Node* new = malloc(sizeof(struct Node));
-	new->x = x;
-	new->y = y;
 
-	while (tmp != NULL) {
-		tmp = tmp->next;
+void set_random_position(int& x, int& y, struct Game* game) {
+	x = random() % game->width;
+	y = random() % game->height;
+}
+
+
+void add_node(struct Snake* snake) {
+	struct Node* new = malloc(sizeof(struct Node));
+	int x = snake->tail->x;
+	int y = snake->tail->y;
+	
+	switch (tail->direction) {
+		case UP:
+			y--;
+			break;
+
+		case DOWN: 
+			y++;
+			break;
+	
+		case LEFT:
+			x++;
+			break;
+
+		case RIGHT: 
+			x--;
+			break;
 	}
 
-	tmp->next = new;
+	new->x = x;
+	ney->y = y;
+	snake->tail->next = new
+	snake->tail = new;
 	snake->length++;
 }
-void init_snake(struct Snake* snake) {
+
+
+void set_starting_position(int& x, int& y, struct Game* game, const int margin) {
+	set_random_position(x, y, game);
+	// Snake is creating in straight line so this will
+	// prevent printing snake's tail which crosses borders
+	if (x < game->width / 2) ? &x += margin : &x -= margin;
+	if (y < game->height / 2) ? &y += margin : &y -= margin;
+}
+
+
+void init_snake(struct Snake* snake, struct Game* game) {
 	const int length = 5;	// starting length
-	enum Direction direction;
-	int x, y;
 
 	for (int i = 0; i < length; i++) {
 		if (i == 0) {
 			struct Node* head = malloc(sizeof(struct Node));
-			head->x = x;
-			head->y = y;
-			snake->direction = direction;
+			set_starting_position(&head->x, &head->y, game, length);
+			head->direction = rand() % 4;
 			snake->head = head;
+			snake->tail = head;
 			snake->length = 1;
 		}
-		else {
-			// TODO
-		}
+		else add_node(snake);
 	}
 }
 
+
 int main() {
+	srand(time(NULL));
 	SDL_Window* window = SDL_CreateWindow(
 		"Snake",			// title	
 		SDL_WINDOWPOS_CENTERED,		// horizontal position	
@@ -71,8 +105,8 @@ int main() {
 	SDL_Event event;
 	SDL_Surface* main_surface = SDL_GetWindowSurface(window);
 	struct Game game = {
-		(WIN_WIDTH - 2) / SIZE,
-		(WIN_HEIGHT - 2) / SIZE,
+		(WIN_WIDTH - 2 * SIZE) / SIZE,
+		(WIN_HEIGHT - 2 * SIZE) / SIZE,
 		0
 	};
 	
